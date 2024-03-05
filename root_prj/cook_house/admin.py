830bad121232efcef25610036b236197a9901842
Anton <anton6733@gmail.com>
2024-03-05 06:57:42 +0200

    some tables

--- root_prj/cook_house/admin.py
+++ root_prj/cook_house/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CategoryRecipe, Recipe
+
+
+@admin.register(CategoryRecipe)
+class CategoryRecipeAdmin(admin.ModelAdmin):
+    list_display = ("pk","title")
+    ordering = ["pk","title"]
+    list_filter = ["title"]
+
--- root_prj/cook_house/models.py
+++ root_prj/cook_house/models.py
@@ -1,4 +1,32 @@
+import pdb
+
 from django.db import models
+from django.utils.translation import gettext_lazy as gtl
+
 
 # Create your models here.
 
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    review = models.TextField()
+    algorythm = models.TextField()
+    time_estimate = models.IntegerField()
+    preview = models.ImageField()
+    author = models.ForeignKey('Users')
+
+
+class CategoryRecipe(models.Model):
+    class Categories(models.TextChoices):
+        SNACKS = "snacks", gtl("Салаты и Закуски")
+        FIRST = 'first_dishes', gtl("Супы")
+        MAIN = 'main_dishes', gtl("Второе")
+        BAKING = 'baking', gtl("Хлеб и Выпечка")
+        DESSERTS = 'desserts', gtl("Десерты")
+        DRINKS = 'drinks', gtl("Безалкогольные напитки")
+        POISON = 'alco', gtl("Пойло")
+
+    title = models.CharField(choices=Categories, max_length=20)
+    default_image = models.ImageField(null=True)
+    def __str__(self):
+        return self.get_title_display()
+# class
