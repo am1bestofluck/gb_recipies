3f81487e40fdb4ea74d8308826510495fc08e1bb
Anton <anton6733@gmail.com>
2024-07-16 12:16:17 +0000

    prg

---
+++ root_prj/support_stuff/install_git.sh
@@ -0,0 +1,6 @@
+# first, update
+sudo apt-get update
+#then install git itself
+echo "installing git" | sudo apt-get install git
+echo "ok"? | git --version
+
---
+++ root_prj/support_stuff/install_nginx.sh
@@ -0,0 +1,5 @@
+
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+sudo tee /etc/apt/sources.list.d/nginx.list
+sudo apt update
+sudo apt install nginx
