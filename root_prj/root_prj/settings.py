b3a6680bd5cb136654f516df54e849690a8dcc5f
Anton <optimavar310am@gmail.com>
2024-07-22 16:19:47 +0300

    secret key

--- root_prj/root_prj/settings.py
+++ root_prj/root_prj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from django.conf import settings
+from dotenv import dotenv_values, load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+load_dotenv('../.env')
+SECRET_KEY = dotenv_values("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
